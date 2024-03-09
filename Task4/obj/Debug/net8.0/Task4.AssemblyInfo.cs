ae302a7c5e7a2009c7cbd643e2fccf3091f4505f
AntonBaglaev <abaglaev@yandex.ru>
2024-03-09 13:36:07 +0300

    fix

--- Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bee41cb4341d2ce75f130aead481000b1d8622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97acd069b0611a02a8fc2a2a3befcd8055ebd505")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
